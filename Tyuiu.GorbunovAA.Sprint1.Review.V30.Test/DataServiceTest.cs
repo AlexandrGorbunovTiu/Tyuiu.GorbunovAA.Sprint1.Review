6f5386f7691b368993cee1ad74422e38d868e1bf
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-10-06 23:31:52 +0500

    Add project files.

---
+++ Tyuiu.GorbunovAA.Sprint1.Review.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorbunovAA.Sprint1.Review.V30.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint1.Review.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 0;
+            int y = 1;
+            var otvet = 1.2; 
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(res, otvet);
+
+        }
+    }
+}
---
+++ Tyuiu.GorbunovAA.Sprint1.Review.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorbunovAA.Sprint1.Review.V30.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint1.Review.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double x, y;
+            Console.Title = "Спринт #1 │ Выполнил: Горбунов А.А. │ ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: SprintReview                                                      *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Вынолнил: Горбунов Александр Александрович │ ИИПб-23-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое                         *");
+            Console.WriteLine("* значение z и округляет его до 3 знаков после запятой                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Y = " + y);
+            Console.WriteLine("Результат: Z = " + ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
